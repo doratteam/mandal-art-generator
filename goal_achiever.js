bf3dd325cfae5e03e4f0c690679bd32fd6ac013f
jameswhang <sungyoonwhang2017@u.northwestern.edu>
2016-01-29 04:32:13 -0600

    in process of implementing goal switching

--- goal_achiever.js
+++ goal_achiever.js
@@ -1,6 +1,8 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
+  Session.setDefault('isDone', false);
+  Session.setDefault('mainGoalSet', false);
 
   Template.download.helpers({
     counter: function () {
@@ -11,15 +13,31 @@ if (Meteor.isClient) {
   Template.download.events({
     'click button': function () {
       // increment the counter when button is clicked
+      if (Session.get('isDone')) {
+        alert('TODO: Download');
+      } else {
+        alert("You're not done yet!");
+      }
       Session.set('counter', Session.get('counter') + 1);
     }
   });
 
+  Template.goalGrid.helpers({
+    isDone: function() {
+      return Session.get('isDone');
+    },
+  });
+
   Template.goalGrid.events({
     'click button': function(e, template) {
-      console.log(template);
-      var mainGoal = template.find('.mainGoal');
-      console.log(mainGoal);
+      //console.log(template);
+      var mainGoal = template.find('.mainGoal').textContent,
+        subGoals = template.findAll('.subGoal');
+
+        Session.set('mainGoal', mainGoal);
+        Session.set('subGoals', subGoals);
+
+        nextGoal();
     }
   });
 }
@@ -29,3 +47,23 @@ if (Meteor.isServer) {
     // code to run on server at startup
   });
 }
+
+var parseInput = function(someStr) {
+  return someStr.replace(' ', '').replace('\t', '');
+};
+
+var newGoal = function(goalStr) {
+  $('.text').fadeOut(800);
+  $('.mainGoal').append(goalStr);
+  /*
+  $('.mainGoal').fadeOut(400, function() {
+    $('.subGoal').fadeOut();
+  });
+  */
+};
+
+var nextGoal = function(subGoalArray, template) {
+  var mainGoal = subGoalArray[0].textContent;
+  console.log(mainGoal);
+  newGoal(mainGoal);
+};
